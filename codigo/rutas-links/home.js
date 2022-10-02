49c898ff2fa8fd492af8d06469cc3b828fa3d3d3
Nelson Sepulveda <nelsonlautarosepulveda@gmail.com>
2022-10-02 13:07:09 -0300

    FRONT

--- codigo/rutas-links/home.js
+++ codigo/rutas-links/home.js
@@ -1,12 +1,18 @@
 const express=require('express');
 const router=express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/recepcionista',(req,res)=>{
     res.render('layouts/index-recepcionista')
 })
 
 router.get('/sigin',(req,res)=>{
     res.render('layouts/sigin')
 })
+
+router.get('/tecnico',(req,res)=>{
+    res.render('layouts/index-tecnico')
+})
+
+
 
 module.exports=router;
